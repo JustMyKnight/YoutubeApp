870b6953a69ebbcbe44fd84f457f983ab08a583d
Admin <admin@Mac-Admin.local>
2015-05-21 05:52:31 +0300

    Initial Commit

---
+++ YouTubeAPP/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YouTubeAPP
+//
+//  Created by Admin on 21.05.15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YouTubeAPP/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YouTubeAPP
+//
+//  Created by Admin on 21.05.15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
